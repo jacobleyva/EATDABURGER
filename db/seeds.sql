9d26beffd22e6401c904638fa4d5d0ae908e3963
unknown <kaijuxtitan@gmail.com>
2019-05-21 16:08:22 -0700

    working routes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+ USE burgers_db;
+ INSERT INTO burgers (id, burger_name, devoured)
+ VALUES (1, 'Bacon Cheese', TRUE), (2, 'MEGA BURGER', FALSE), (3, 'Daves Special', FALSE);
--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,36 @@ module.exports = app => {
 /**
  * COPY PASTA FROM OLDER FILE 
  */
-  //   app.post('/users', (req, res) => {
-//     db.query('INSERT INTO users SET ?', req.body, e => {
-//       if (e) throw e
-//       res.send('ok')
-//     })
-//   })
-//   // Read
-//   app.get('/users', (req, res) => {
-//     db.query('SELECT * FROM users', (e, users) => {
-//       if (e) throw e
-//       res.json(users)
-//     })
-//   })
-//   // Update
-//   app.put('/users/:id', (req, res) => {
-//     db.query('UPDATE users SET ? WHERE ?', [req.body, { id: req.params.id }], e => {
-//       if (e) throw e
-//       res.send('ok')
-//     })
-//   })
-//   // Delete
-//   app.delete('/users/:id', (req, res) => {
-//     db.query('DELETE FROM users WHERE ?', { id: req.params.id }, e => {
-//       if (e) throw e
-//       res.send('ok')
-//     })
-//   })
-// }
+  //MAIN
+  app.get('/', (req, res) => {
+    return res.render('index')
+  })
+  //POST
+    app.post('/burger', (req, res) => {
+    db.query('INSERT INTO burgers SET ?', req.body, e => {
+      if (e) throw e
+      res.send('ok')
+    })
+  })
+  // Read
+  app.get('/burger', (req, res) => {
+    db.query('SELECT * FROM burgers', (e, users) => {
+      if (e) throw e
+      res.json(burgers)
+    })
+  })
+  // Update
+  app.put('/burger/:id', (req, res) => {
+    db.query('UPDATE burgers SET ? WHERE ?', [req.body, { id: req.params.id }], e => {
+      if (e) throw e
+      res.send('ok')
+    })
+  })
+  // Delete
+  app.delete('/burger/:id', (req, res) => {
+    db.query('DELETE FROM burgers WHERE ?', { id: req.params.id }, e => {
+      if (e) throw e
+      res.send('ok')
+    })
+  })
+}
